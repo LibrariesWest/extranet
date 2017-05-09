5a6ca930fc836c1944de084dd9dc05d84bc12eef
Dave <dave_roweuk@yahoo.co.uk>
2017-05-09 23:21:44 +0100

    Primarily working on usage dashboard

---
+++ chart-config.js
@@ -0,0 +1,15 @@
+﻿var rootdataurl = 'data/';
+
+var colours = {
+    'Bath and North East Somerset': { colour: [37, 164, 69] },
+    'Somerset': { colour: [184, 0, 80] },
+    'North Somerset': { colour: [206, 245, 242] },
+    'South Gloucestershire': { colour: [0, 10, 139] },
+    'Dorset': { colour: [239, 155, 31] },
+    'Poole': { colour: [0, 148, 170] },
+    'Bristol': { colour: [255, 204, 0] }
+};
+
+var issuesurl = rootdataurl + 'issues.csv';
+var issuesbylibraryurl = rootdataurl + 'issuesbylibrary.csv';
+var holdsurl = rootdataurl + 'holds.csv';
